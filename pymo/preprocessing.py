Fixed the rotmat calculation

pymo/preprocessing.py
@@ -63,12 +63,12 @@ class MocapParameterizer(BaseEstimator, TransformerMixin):
             pos_df = pd.DataFrame(index=euler_df.index)
 
             # Copy the root rotations into the new DataFrame
-            rxp = '%s_Xrotation'%track.root_name
-            ryp = '%s_Yrotation'%track.root_name
-            rzp = '%s_Zrotation'%track.root_name
-            pos_df[rxp] = pd.Series(data=euler_df[rxp], index=pos_df.index)
-            pos_df[ryp] = pd.Series(data=euler_df[ryp], index=pos_df.index)
-            pos_df[rzp] = pd.Series(data=euler_df[rzp], index=pos_df.index)
+            # rxp = '%s_Xrotation'%track.root_name
+            # ryp = '%s_Yrotation'%track.root_name
+            # rzp = '%s_Zrotation'%track.root_name
+            # pos_df[rxp] = pd.Series(data=euler_df[rxp], index=pos_df.index)
+            # pos_df[ryp] = pd.Series(data=euler_df[ryp], index=pos_df.index)
+            # pos_df[rzp] = pd.Series(data=euler_df[rzp], index=pos_df.index)
 
             # List the columns that contain rotation channels
             rot_cols = [c for c in euler_df.columns if ('rotation' in c)]
@@ -105,27 +105,34 @@ class MocapParameterizer(BaseEstimator, TransformerMixin):
                                   f[1]['%s_Yposition'%joint], 
                                   f[1]['%s_Zposition'%joint]] for f in pc.iterrows()]
 
-                #euler_values = [[0,0,0] for f in rc.iterrows()]
-                #pos_values = [[0,0,0] for f in pc.iterrows()] 
+                #euler_values = [[0,0,0] for f in rc.iterrows()] #for deugging
+                #pos_values = [[0,0,0] for f in pc.iterrows()] #for deugging
+                
                 # Convert the eulers to rotation matrices
-                rotmats = np.asarray([Rotation(f, 'euler', from_deg=True).rotmat.T for f in euler_values])                  
-                tree_data[joint]=[[],[]] 
+                rotmats = np.asarray([Rotation([f[0], f[1], f[2]], 'euler', from_deg=True).rotmat for f in euler_values])                  
+                tree_data[joint]=[
+                                    [], # to store the rotation matrix
+                                    []  # to store the calculated position
+                                 ] 
 
                 if track.root_name == joint:
                     tree_data[joint][0] = rotmats
-                    tree_data[joint][1] = np.add(pos_values, track.skeleton[joint]['offsets'])
-                    #tree_data[joint][1] = pos_values
+                    # tree_data[joint][1] = np.add(pos_values, track.skeleton[joint]['offsets'])
+                    tree_data[joint][1] = pos_values
                 else:
+                    # for every frame i, multiply this joint's rotmat to the rotmat of its parent
                     tree_data[joint][0] = np.asarray([np.matmul(rotmats[i], tree_data[parent][0][i]) 
                                                       for i in range(len(tree_data[parent][0]))])
 
+                    # add the position channel to the offset and store it in k, for every frame i
                     k = np.asarray([np.add(pos_values[i],  track.skeleton[joint]['offsets'])
                                     for i in range(len(tree_data[parent][0]))])
 
-
+                    # multiply k to the rotmat of the parent for every frame i
                     q = np.asarray([np.matmul(k[i], tree_data[parent][0][i]) 
                                     for i in range(len(tree_data[parent][0]))])
 
+                    # add q to the position of the parent, for every frame i
                     tree_data[joint][1] = np.asarray([np.add(q[i], tree_data[parent][1][i])
                                                       for i in range(len(tree_data[parent][1]))])
 

pymo/rotation_tools.py
@@ -33,20 +33,32 @@ class Rotation():
             beta = deg2rad(beta)
             gamma = deg2rad(gamma)
         
+        ca = math.cos(alpha)
+        cb = math.cos(beta)
+        cg = math.cos(gamma)
+        sa = math.sin(alpha)
+        sb = math.sin(beta)
+        sg = math.sin(gamma)        
+
         Rx = np.asarray([[1, 0, 0], 
-              [0, math.cos(alpha), -math.sin(alpha)], 
-              [0, math.sin(alpha), math.cos(alpha)]
+              [0, ca, sa], 
+              [0, -sa, ca]
               ])
 
-        Ry = np.asarray([[math.cos(beta), 0, math.sin(beta)], 
+        Ry = np.asarray([[cb, 0, -sb], 
               [0, 1, 0],
-              [-math.sin(beta), 0, math.cos(beta)]])
+              [sb, 0, cb]])
 
-        Rz = np.asarray([[math.cos(gamma), -math.sin(gamma), 0],
-              [math.sin(gamma), math.cos(gamma), 0],
+        Rz = np.asarray([[cg, sg, 0],
+              [-sg, cg, 0],
               [0, 0, 1]])
 
-        self.rotmat = np.matmul(np.matmul(Rz, Ry), Rx)
+        self.rotmat = np.eye(3)
+
+        self.rotmat = np.matmul(Rx, self.rotmat)
+        self.rotmat = np.matmul(Ry, self.rotmat)
+        self.rotmat = np.matmul(Rz, self.rotmat)
+        # self.rotmat = np.matmul(np.matmul(Rz, Ry), Rx)
    
     def _from_expmap(self, alpha, beta, gamma, params):
         if (alpha == 0 and beta == 0 and gamma == 0):
@@ -126,6 +138,9 @@ class Rotation():
     def to_quat(self):
         #TODO
         pass
+    
+    def __str__(self):
+        return "Rotation Matrix: \n " + self.rotmat.__str__()
     
 
 