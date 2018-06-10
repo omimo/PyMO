935b0b5f6e91d6f8eb0b94f94f1c875618faa8c0
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2018-06-09 23:15:15 -0700

    Raise exceptions

--- pymo/preprocessing.py
+++ pymo/preprocessing.py
@@ -32,8 +32,7 @@ class MocapParameterizer(BaseEstimator, TransformerMixin):
         elif self.param_type == 'position':
             return self._to_pos(X)
         else:
-            raise 'param types: euler, quat, expmap, position'
-
+            raise UnsupportedParamError('Unsupported param: %s. Valid param types are: euler, quat, expmap, position' % self.param_type)
 #        return X
     
     def inverse_transform(self, X, copy=None): 
@@ -42,13 +41,12 @@ class MocapParameterizer(BaseEstimator, TransformerMixin):
         elif self.param_type == 'expmap':
             return self._expmap_to_euler(X)
         elif self.param_type == 'quat':
-            raise 'quat2euler is not supported'
+            raise UnsupportedParamError('quat2euler is not supported')
         elif self.param_type == 'position':
-            # raise 'positions 2 eulers is not supported'
             print('positions 2 eulers is not supported')
             return X
         else:
-            raise 'param types: euler, quat, expmap, position'
+            raise UnsupportedParamError('Unsupported param: %s. Valid param types are: euler, quat, expmap, position' % self.param_type)
 
     def _to_pos(self, X):
         '''Converts joints rotations in Euler angles to joint positions'''
@@ -731,3 +729,6 @@ class TemplateTransform(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         return X
 
+class UnsupportedParamError(Exception):
+    def __init__(self, message):
+        self.message = message
