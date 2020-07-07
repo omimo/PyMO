afbc39ba410712e3e5edb56f41a362c4a45fd947
Omid Alemi <omid@omid.al>
2020-07-07 10:40:17 -0700

    added the c3d parser + moved the parsers into a separate folder

---
+++ pymo/parsers/__init__.py
@@ -0,0 +1,2 @@
+from .bvh_parser import BVHParser
+from .ezc3d_parser import C3DParser
---
+++ pymo/parsers/ezc3d_parser.py
@@ -0,0 +1,76 @@
+'''
+C3D Parser Class
+
+
+By Omid Alemi
+Created: July, 2020
+'''
+
+import numpy as np
+import pandas as pd
+import ezc3d 
+
+from pymo.data import Joint, MocapData
+
+
+class C3DParser():
+    '''
+    Parses a C3D file using the ezc3d library and creates a 
+    PyMo.MocapData object.
+    '''
+
+    def __init__(self, filename):
+        self.data  = self._parse(filename)
+
+
+    def _parse(self, filename):
+        ezc = ezc3d.c3d(filename)
+
+        param_points = ezc['header']['points']
+
+        #TODO: Handle the cases the file does not have these info
+        marker_labels = ezc['parameters']['POINT']['LABELS']['value']
+        marker_descriptions = ezc['parameters']['POINT']['DESCRIPTIONS']['value']
+
+
+        mocap_data = MocapData()        
+        mocap_data.skeleton = None
+        mocap_data.root_name = None
+
+        
+        mocap_data.channel_names = marker_labels
+        mocap_data.framerate = param_points['frame_rate']
+        mocap_data.metadata = {}
+        mocap_data.metadata['marker_descriptions'] = marker_descriptions
+
+        
+        points = ezc['data']['points']
+
+        
+        
+        values = []
+        column_names = []
+
+        for m in range(len(marker_labels)): #TODO: Make the ordering more clear to the user
+            column_names.append('%s_Xposition'%marker_labels[m])
+            column_names.append('%s_Zposition'%marker_labels[m])
+            column_names.append('%s_Yposition'%marker_labels[m])
+
+        for f in range(points.shape[2]):
+            frame_values = []
+
+            for m in range(points.shape[1]):
+                frame_values.append(points[0,m,f])
+                frame_values.append(points[1,m,f])
+                frame_values.append(points[2,m,f])
+
+            values.append(frame_values)
+        
+        values = np.asarray(values)
+
+        time_index = pd.to_timedelta([f[0] for f in values], unit='s')
+
+        mocap_data.values = pd.DataFrame(data=values, index=time_index, columns=column_names)
+
+        return mocap_data
+        
