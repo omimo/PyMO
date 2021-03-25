e294100d756804b87425ae5845d4f4fd58d72c10
zhaoyafei <zhaoyafei@baidu.com>
2021-03-25 17:43:31 +0800

    add MANIFEST.in to include pymo/mocapplayer/* when running setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://omid.al/projects/PyMO.html",
     packages=setuptools.find_packages(),
+    include_package_data=True,    # include files in MANIFEST.in
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
+    install_requires=[    # dependencies
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'scikit-learn'
+    ]
 )
